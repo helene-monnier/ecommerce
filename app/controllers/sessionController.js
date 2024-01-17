86e5f97247943e76a5ebe6a82d52a85569073ffb
helene-monnier <monnier.helene@outlook.com>
2024-01-17 12:10:10 +0100

    feature: methode register ok

--- app/controllers/sessionController.js
+++ app/controllers/sessionController.js
@@ -11,19 +11,41 @@ const sessionController = {
             const { email, password } = req.body;
             // !! Votre code à partir d'ici
 
-            // On récupère user avec le role
-            // Est-ce que l'utilisateur existe en BDD ?
-            // Sélectionner user avec email et inclure le role, si on ne le trouve pas :
-            //      on envoie un message d'erreur dans un objet:  {error: "Utilisateur ou mot de passe incorrect"} et on render `login` en lui passant l'erreur
-            // Sinon on continue.
+            // est-ce que l'user existe en bdd ? 
+            const userFounded = await User.findOne({
+                where: {
+                    email, // Sélectionner user avec email
+                },
+                include: [{
+                    association: 'role', // inclure son role
+                }]
+            });
+
+            if (!userFounded) { // si on ne trouve pas
+                res.render('login', { 
+                    errorMessage: "Utilisateur ou mot de passe incorrect",
+                }); //on envoie un message d'erreur dans un objet:  {error: "Utilisateur ou mot de passe incorrect"} et on render `login` en lui passant l'erreur
+                return; // sinon on continue
+            }
 
             // Le mot de passe est il correct ?
-            // On compare le mots de passe du formulaire avec celui de l'utilisateur
-            //      Si le mot de passe est incorrect : on envoie un message d'erreur dans un objet:  {error: "Utilisateur ou mot de passe incorrect"} et on render `login` en lui passant l'erreur
-
+            const isSamePassword = await bcrypt.compare(password, userFounded.password);// On compare le mots de passe du formulaire avec celui de l'utilisateur
+            
+            if(!isSamePassword) {//      Si le mot de passe est incorrect :
+                //on envoie un message d'erreur dans un objet:  {error: "Utilisateur ou mot de passe incorrect"} et on render `login` en lui passant l'erreur
+                res.render('login', { 
+                    errorMessage: 'Email ou mot de passe incorrect',
+                });
+                return; // sinon on continue
+            }
+             
             // On ajoute user a la session
+            req.session.userId = userFounded.id;
+            console.log(req.session.userId)
+            console.log(userFounded)
+            
 
-            // On enlève le mot de passe de la session.
+            // On enlève le mot de passe de la session => je n'ai conservé que l'id
 
             // !! Ne pas modifier cette ligne
             res.redirect('/');
@@ -35,6 +57,8 @@ const sessionController = {
 
     logout: (req, res) => {
         // !! Votre code ici
+        // Je détruis la session
+        req.session.destroy();
         res.redirect('/');
     },
 };
